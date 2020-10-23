45e28be12718866fdc85a97df333c089f97c4f01
David Fasanya <david.fasanya@ttu.edu>
2020-10-23 00:32:34 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by David Fasanya on 10/23/20.
+//  Copyright © 2020 fasanyadavid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
